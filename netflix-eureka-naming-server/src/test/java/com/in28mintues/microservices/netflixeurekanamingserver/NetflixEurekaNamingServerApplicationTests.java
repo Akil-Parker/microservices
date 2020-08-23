9cf462ba0f1ee9ac8e87b0ac238b07c8b18c3d16
Akil Vanchu <akilwanchoo@gmail.com>
2020-08-23 14:06:58 +0530

    first commit

---
+++ netflix-eureka-naming-server/src/test/java/com/in28mintues/microservices/netflixeurekanamingserver/NetflixEurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28mintues.microservices.netflixeurekanamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixEurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
